cb26da6be1007f69fcaeeb43340cc471d6a0d604
Michallis Pashidis <michallis@trust1team.com>
2019-10-20 11:17:43 +0200

    update coords

--- src/com/t1t/examples/TestApplication.java
+++ src/com/t1t/examples/TestApplication.java
@@ -3,10 +3,15 @@ package com.t1t.examples;
 public class TestApplication {
 
     public static void main (String[]args){
-        double[] coords={5.0 , 0.0};
+        double[] coords={5.0 , 0.0, 3.0};
         ArrayLocation accra=new ArrayLocation(coords);
         coords[0]=32.9 ;
         coords[1]=-117.2;
-        System.out.println(coords[0]);
+        for (double coord : coords) {
+            System.out.println(coord);
+        }
+/*        System.out.println(coords[0]);
+        System.out.println(coords[2]);
+        System.out.println(coords[3]);*/
     }
 }
